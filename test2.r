f781098f9f832c51a7bd4de2288ab19764fbe410
MaiWind <maike.wintgens@rwth-aachen.de>
2025-10-24 11:54:58 +0200

    korrekt

--- test2.r
+++ test2.r
@@ -1,7 +1,7 @@
 # test2hallo
 # Hey
 # wie geht es dir
-# Super ich fühle mich wie ein Genial Programmierer!
+# Super ich fühle mich wie ein Genialer Programmierer!
 # super das freut mich sehr! wmdgs? ;)indts 
 # nichts
 # schade
