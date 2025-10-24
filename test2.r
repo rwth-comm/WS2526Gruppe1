1a04664f8d14bb52a070218610e246054fb7415f
MaiWind <maike.wintgens@rwth-aachen.de>
2025-10-24 11:53:51 +0200

    jey

--- test2.r
+++ test2.r
@@ -2,5 +2,6 @@
 # Hey
 # wie geht es dir
 # Super ich fühle mich wie ein Genial Programmierer!
-# super das freut mich sehr! wmdgs? ;) 
+# super das freut mich sehr! wmdgs? ;)indts 
 # nichts
+# schade
