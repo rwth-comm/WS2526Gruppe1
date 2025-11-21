2d6f79e792f7e48123be95269797b8f4cdadefac
Annika Samrock <annika.samrock@rwth-aachen.de>
2025-11-21 13:44:17 +0100

    Übungsdatei mit ein paar notizen zu einigen Maßen

--- Uebung211125.R
+++ Uebung211125.R
@@ -1,10 +1,10 @@
 # Pakete installieren
 
-install.packages("tidyverse")
-install.packages("psych")
-install.packages("plotrix")
-install.packages("remotes")
-remotes::install_github("statisticsforsocialscience/dataforsocialscience")
+#install.packages("tidyverse")
+#install.packages("psych")
+#install.packages("plotrix")
+#install.packages("remotes")
+#remotes::install_github("statisticsforsocialscience/dataforsocialscience")
 
 # Pakete aktivieren
 library(tidyverse)
@@ -14,3 +14,27 @@ library(dataforsocialscience)
 
 # Daten einlesen
 df <- robo_care
+table(df$gender)
+library(ggplot2)
+qplot(df$gender)
+median(df$age)
+quantile(df$age)
+boxplot(df$age)
+
+
+
+# eigene Daten
+quantile(df$privacy_concerns)
+boxplot(df$privacy_concerns)
+mean(df$privacy_concerns)
+# MAD berechnet den durchscnittlichen Abstand zum Mittelwert (standardmäßig Median)
+mad(df$privacy_concerns)
+var(df$privacy_concerns)
+# die Varianz zerstört dadurch, dass sie quadriert wird, die Einheit. Dadurch wird sie kaum interpretierbar. Die Standardabweichung zieht die Wurzel, um das Problem zu lösen.
+SD(df$privacy_concerns)
+# Der Standardfehler (SE) teilt die SD durch die Wurzel aus n. Die SD wird also durch die Wurzel der Stichprobengröße geteilt
+std.error(df$privacy_concerns)
+
+# Maßnahmenpaket psych sammelt lauter tolle Maße
+psych::describe(df)
+# trimmed berechnet den Durchschnitt ohne Extremwerte (oberste und unterste 10 %)
